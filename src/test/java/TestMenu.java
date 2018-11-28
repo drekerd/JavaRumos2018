2aed58953e18f414a2872619a826a5aa30d86f99
drekerd <marioalves.silva17@gmail.com>
2018-11-28 21:49:20 +0000

    AssertUnitTest_1

--- src/test/java/TestMenu.java
+++ src/test/java/TestMenu.java
@@ -1,5 +1,12 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMenu {
+
+    @Test
+    public void HelloTest(){
+        String hello = "hello world";
+        Assert.assertTrue("Hello World".equals(hello));
+    }
 
 }
